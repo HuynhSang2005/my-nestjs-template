adc742d742dbe35d1d8a49a32e0d7bc1d129e94d
HuynhSang2005 <huynhsang060305@gmail.com>
2025-07-19 12:50:54 +0700

    feat(app): enable global zod validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  // Sử dụng ZodValidationPipe global
+  app.useGlobalPipes(new ZodValidationPipe());
 }
 bootstrap();
